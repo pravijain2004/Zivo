d8399d97ef8ff45c5cc3851d193251c2428c7980
Pravi Jain <pravi.jain12@gmail.com>
2026-01-03 16:59:58 +0530

    Chat-container-setup-done

--- Frontend/src/components/NoConversationPlaceholder.jsx
+++ Frontend/src/components/NoConversationPlaceholder.jsx
@@ -1,7 +1,17 @@
-function NoConversationPlaceholder() {
-  return (
-    <div> NoConversationPlaceholder </div>
-  )
-}
+import {MessageCircleIcon} from "lucide-react";
 
-export default NoConversationPlaceholder
+const NoConversationPlaceholder = () => {
+  return (
+    <div className = "flex flex-col items-center justify-center h-full text-center p-6">
+      <div className="size-20 bg-cyan-500/20 rounded-full flex items-center justify-center mb-6">
+      <MessageCircleIcon className="text-slate-400 max-w-md"></MessageCircleIcon>
+      </div>
+      <h3 className="text-xl font-semihold text-slate-200 mb-2">Select a conversation</h3>
+      <p className="text-slate-400 max-w-md">
+        Choose a contact from the sidebar to start chatting or continue a previous conversation.
+      </p>
+    </div>
+  );
+};
+
+export default NoConversationPlaceholder;
