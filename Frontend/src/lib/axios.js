660e9affd4a5da323939cff704a05e178ec98b59
Pravi Jain <pravi.jain12@gmail.com>
2026-07-01 18:10:39 +0530

    added backend live link

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:3000/api",
+  baseURL:"https://zivo-033i.onrender.com",
   withCredentials:true,
 });
